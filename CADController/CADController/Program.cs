f62b0115dd18db5818b2c01025efd340412a28de
Alexey Kalinichenko <kalinichenkoae@gmail.com>
2017-01-15 14:09:58 +0400

    added controller

---
+++ CADController/CADController/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CADController
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
+        }
+    }
+}
